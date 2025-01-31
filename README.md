6edc9adc4f08727b013d9588cb705af2a367d5c1
hamyat <hamyat86@gmail.com>
2025-02-01 00:13:52 +0700

    Update README and enhance bike sharing app: add setup instructions, fix execution counts, and improve rental data handling

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# bike_sharing_analysis
+# Bike Sharing Analysis Dashboard
+
+## Setup Environment - Anaconda
+```
+conda create --name dataviz python=3.10
+conda activate dataviz
+pip install -r requirements.txt
+```
+
+## Setup Environment - Shell/Terminal
+```
+mkdir data_analysis
+cd data_analysis
+pipenv install
+pipenv shell
+pip install -r requirements.tx
+```
+## Run streamlit app
+```
+streamlit run bike_sharing_app.py
+```
--- bike_sharing_app.py
+++ bike_sharing_app.py
@@ -15,37 +15,37 @@ df['dteday'] = pd.to_datetime(df['dteday'])
 
 # Bike Monthly Rentals
 def bike_monthly_rentals(df):
-    monthly_rentals = df.groupby('mnth')['cnt'].sum()
-    return monthly_rentals
+    monthly_rentals_count = df.groupby('mnth')['cnt'].sum()
+    return monthly_rentals_count
 
 # Bike Daily Rentals
 def bike_daily_rentals(df):
-    daily_rentals = df.groupby('dteday')[['cnt', 'casual', 'registered']].sum()
-    return daily_rentals
+    daily_rentals_count = df.groupby('dteday')[['cnt', 'casual', 'registered']].sum()
+    return daily_rentals_count
 
 # Bike Hourly Rentals
 def bike_hourly_rentals(df):
-    hourly_rentals = df.groupby('hr')['cnt'].sum()
+    hourly_rentals_count = df.groupby('hr')['cnt'].sum()
     fig, ax = plt.subplots()
-    hourly_rentals.plot(kind='bar', ax=ax)
+    hourly_rentals_count.plot(kind='bar', ax=ax)
     ax.set_xlabel('Hour of the Day')
     ax.set_ylabel('Number of Rentals')
     return fig
 
 # Bike by Season
 def bike_by_season(df):
-    season_rentals = df.groupby('season')['cnt'].sum()
+    season_rentals_count = df.groupby('season')['cnt'].sum()
     fig, ax = plt.subplots()
-    season_rentals.plot(kind='bar', ax=ax)
+    season_rentals_count.plot(kind='bar', ax=ax)
     ax.set_xlabel('Season')
     ax.set_ylabel('Number of Rentals')
     return fig
 
 # Bike by Weather
 def bike_by_weather(df):
-    weather_rentals = df.groupby('weathersit')['cnt'].sum()
+    weather_rentals_count = df.groupby('weathersit')['cnt'].sum()
     fig, ax = plt.subplots()
-    weather_rentals.plot(kind='bar', ax=ax)
+    weather_rentals_count.plot(kind='bar', ax=ax)
     ax.set_xlabel('Weather Situation')
     ax.set_ylabel('Number of Rentals')
     return fig
@@ -53,34 +53,35 @@ def bike_by_weather(df):
 # Bike by Temperature
 def bike_by_temp(df):
     fig, ax = plt.subplots()
-    sns.histplot(df['temp'], kde=True, ax=ax)
+    temp_rentals = df.groupby('temp')['cnt'].sum()
+    temp_rentals.plot(kind='bar', ax=ax)
     ax.set_xlabel('Temperature')
     ax.set_ylabel('Number of Rentals')
     return fig
 
 # Bike by Weekday
 def bike_by_weekday(df):
-    weekday_rentals = df.groupby('weekday')['cnt'].sum()
+    weekday_rentals_count = df.groupby('weekday')['cnt'].sum()
     fig, ax = plt.subplots()
-    weekday_rentals.plot(kind='bar', ax=ax)
+    weekday_rentals_count.plot(kind='bar', ax=ax)
     ax.set_xlabel('Day of the Week')
     ax.set_ylabel('Number of Rentals')
     return fig
 
 # Bike by Holiday
 def bike_by_holiday(df):
-    holiday_rentals = df.groupby('holiday')['cnt'].sum()
+    holiday_rentals_count = df.groupby('holiday')['cnt'].sum()
     fig, ax = plt.subplots()
-    holiday_rentals.plot(kind='bar', ax=ax)
+    holiday_rentals_count.plot(kind='bar', ax=ax)
     ax.set_xlabel('Holiday')
     ax.set_ylabel('Number of Rentals')
     return fig
 
 # Bike by Working Day
 def bike_by_workingday(df):
-    workingday_rentals = df.groupby('workingday')['cnt'].sum()
+    workingday_rentals_count = df.groupby('workingday')['cnt'].sum()
     fig, ax = plt.subplots()
-    workingday_rentals.plot(kind='bar', ax=ax)
+    workingday_rentals_count.plot(kind='bar', ax=ax)
     ax.set_xlabel('Working Day')
     ax.set_ylabel('Number of Rentals')
     return fig
@@ -102,27 +103,27 @@ max_date = df['dteday'].max()
 
 # Sidebar
 with st.sidebar:
-    # Menambahkan logo dari <a href="https://www.flaticon.com/free-icons/rental" title="rental icons">Rental icons created by surang - Flaticon</a>
+    # Menambahkan logo dari 
     st.title('Bike Sharing Dashboard')
-    st.image("https://github.com/hamyat/bike_sharing_analysis/blob/main/rent.png")
+    st.image("https://raw.githubusercontent.com/hamyat/bike_sharing_analysis/main/rent.png", width=150)
     st.subheader('Filters')
     # Mengambil start date dan end date dari user
     start_date = st.date_input('Start Date', min_date)
     end_date = st.date_input('End Date', max_date)
 
-    # start_date, end_date = st.date_input(
-    #     label='Rentang Waktu',min_value=min_date,
-    #     max_value=max_date,
-    #     value=[min_date, max_date]
-    # )
+    st.markdown('<a href="https://www.flaticon.com/free-icons/rental" title="rental icons">Rental icons created by surang - Flaticon</a>', unsafe_allow_html=True)
+start_date = pd.to_datetime(start_date)
+end_date = pd.to_datetime(end_date)
 
 main_df = df[(df['dteday'] >= str(start_date)) & (df['dteday'] <= str(end_date))]
+daily_rentals_count = bike_daily_rentals(df)
+daily_rentals = bike_daily_rentals(main_df).resample('D').sum()
 
-daily_rentals = bike_daily_rentals(main_df)
-monthly_rentals = bike_monthly_rentals(main_df)
+weather_rentals_count = bike_by_weather(main_df)
+monthly_rentals_count = bike_monthly_rentals(main_df)
 season_rentals = bike_by_season(main_df)
 weather_rentals = bike_by_weather(main_df)
-hourly_rentals = bike_hourly_rentals(main_df)
+hourly_rentals_count = bike_hourly_rentals(main_df)
 
 # Dashboard
 # Set the title of the dashboard
@@ -133,10 +134,10 @@ st.subheader('Exploratory Data Analysis')
 st.write('## Bike Rentals Over Time')
 st.line_chart(daily_rentals)
 st.write('## Monthly Bike Rentals')
-st.bar_chart(monthly_rentals)
+st.bar_chart(monthly_rentals_count)
 st.write('## Hourly Bike Rentals')
-st.pyplot(hourly_rentals)
-st.write('## Bike Rentals by Season')
+st.pyplot(hourly_rentals_count)
+st.pyplot(weather_rentals_count)
 st.pyplot(season_rentals)
 st.write('## Bike Rentals by Weather Situation')
 st.pyplot(weather_rentals)
@@ -145,6 +146,10 @@ st.pyplot(bike_by_temp(main_df))
 st.write('## Bike Rentals by Day of the Week')
 st.pyplot(bike_by_weekday(main_df))
 st.write('## Bike Rentals by Holiday')
+st.write('## Bike Rentals by Holiday')
 st.pyplot(bike_by_holiday(main_df))
 st.write('## Bike Rentals by Working Day')
 st.pyplot(bike_by_workingday(main_df))
+
+# footer
+st.write('© 2021 Hamyat')
